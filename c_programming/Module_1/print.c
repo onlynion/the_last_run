430a3142c4c73f88c6102b3f0678af410b2a64c9
onlynion <sadikalwahid2018@gamil.com>
2025-01-18 01:10:43 +0600

    18-01-2025

---
+++ c_programming/Module_1/print.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello Prity \n");
+    printf("This is the last run");
+    printf("\n\\");
+    printf("\n%%");
+    return 0;
+}
+
+// \n ---> new line
+// \t ---> for tab
+
+// "\" & "%" are special character if i want to print them i have to write them twice
